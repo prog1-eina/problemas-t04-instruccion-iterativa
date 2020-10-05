d7cb7a90465121f0fc933aea9a281c70c447dc6f
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-10-05 20:25:34 +0200

    Update 3-perfecto.cpp

--- 3-perfecto.cpp
+++ 3-perfecto.cpp
@@ -1,6 +1,6 @@
 ﻿/*
  * Autores: Javier Martínez y Miguel Ángel Latre
- * Última revisión: 9-10-2019
+ * Última revisión: 5-10-2020
  * Resumen: Programa interactivo que indica si un número introducido por el
  *          usuario es o no perfecto.
  *          4.ª clase de problemas de Programación 1
@@ -9,12 +9,11 @@
 using namespace std;
 
 /*
- * Pre:  n > 0
- * Post: Ha devuelto true si y solo si «n» es un número perfecto.
+ * Devuelve true si y solo si «n» es un número perfecto.
  */
-bool esPerfecto(int n) {
-    int suma = 0;
-    for (int divisor = 1; divisor <= n/2; divisor++) {
+bool esPerfecto(unsigned int n) {
+    unsigned int suma = 0;
+    for (unsigned int divisor = 1; divisor <= n/2; divisor++) {
         if (n % divisor == 0) {
             suma += divisor;
         }
@@ -23,11 +22,10 @@ bool esPerfecto(int n) {
 }
 
 /*
- * Pre:  ---
- * Post: Ha pedido reiteradamente al usuario un número entero positivo y ha
- *       devuelto el primer número entero positivo introducido por el usuario.
+ * Pide reiteradamente al usuario un número entero positivo y devuelve el primer
+ * número entero positivo introducido por el usuario.
  */ 
-int pedirNumeroNatural() {
+unsigned int pedirNumeroNatural() {
     cout << "Escriba un número natural: ";
     int n;
     cin >> n;
@@ -41,11 +39,11 @@ int pedirNumeroNatural() {
 }
 
 /*
- * Programa que solicita al usuario un número natural y le ha informado acerca de
- * si dicho número es perfecto o no.
+ * Programa que solicita al usuario un número natural y le ha informado acerca 
+ * de si dicho número es perfecto o no.
  */ 
 int main() {
-    int n = pedirNumeroNatural();
+    unsigned int n = pedirNumeroNatural();
     
     cout << n;
     if (!esPerfecto(n)) {
